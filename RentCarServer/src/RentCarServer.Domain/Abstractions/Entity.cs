6acf7714dcdf02e452b0b2f58c6dbccca42beae9
OguzhanAcer <oguzhanturgut611@gmail.com>
2025-07-28 21:40:57 +0300

    Initial commit

---
+++ RentCarServer/src/RentCarServer.Domain/Abstractions/Entity.cs
@@ -0,0 +1,8 @@
+﻿namespace RentCarServer.Domain.Abstractions;
+
+public abstract class Entity
+{
+    public Guid Id { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
+}
